a56aa106b4779f2fb84bb12bd6e1508d2f8e225d
Matt Hensen <matthew.hensen92@gmail.com>
2022-10-30 21:42:10 -0500

    started sorting algos

---
+++ sorting/BubbleSort.js
@@ -0,0 +1,15 @@
+export default function bubbleSort(arr) {
+  let swapped = true;
+
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; ++i) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]; // swap
+        swapped = true;
+      }
+    }
+  }
+
+  return arr;
+}
